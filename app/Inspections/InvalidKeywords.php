687c758684e6162dbaede5b09d4ddbf0f36f9c71
Ruman Saleem <ruman63@gmail.com>
2017-12-20 02:14:05 +0530

    Episode 50: Spam Detection Part 2

---
+++ app/Inspections/InvalidKeywords.php
@@ -0,0 +1,21 @@
+<?php 
+namespace App\Inspections;
+
+use Exception;
+
+class InvalidKeywords
+{
+    protected $invalids = [
+        'Yahoo Customer Support',
+        'Fcuk'
+    ];
+    
+    public function detect($body)
+    {
+        foreach ($this->invalids as $keyword) {
+            if (stripos($body, $keyword) !== false) {
+                throw new Exception();
+            }
+        }
+    }
+}
--- tests/Unit/SpamTest.php
+++ tests/Unit/SpamTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Inspections\Spam;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -9,14 +10,24 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class SpamTest extends TestCase
 {
     /** @test */
-    public function it_validates_body_for_spam()
+    public function it_checks_for_invalid_keywords()
     {
-        $spam = new \App\Spam;
+        $spam = new Spam;
 
         $this->assertFalse($spam->detect('A Nice spamless message'));
 
         $this->expectException(\Exception::class);
         
         $spam->detect('A Yahoo Customer Support');
+    }
+
+    /** @test */
+    public function it_checks_for_key_held_down()
+    {
+        $spam = new Spam;
+
+        $this->expectException(\Exception::class);
+        
+        $spam->detect('Hello thereeeeeeeeeeeeee!');
     }
 }
